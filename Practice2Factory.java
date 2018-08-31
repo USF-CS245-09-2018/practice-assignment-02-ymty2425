2dc334501c0ebef81ae7aae3ef543884f374f950
David Guy Brizan <dgrizan>
2018-08-31 10:18:19 -0700

    Initial commit

---
+++ Practice2Factory.java
@@ -0,0 +1,13 @@
+public class Practice2Factory {
+	
+	public Practice2Search getSearch(String searchtype) {
+		if (searchtype.contains("linear"))
+			return new LinearSearch();
+		else if (searchtype.contains("binary") && searchtype.contains("recursive"))
+			return new BinaryRecursiveSearch();
+		else if (searchtype.contains("binary") && searchtype.contains("iterative"))
+			return new BinaryIterativeSearch();
+		return null;
+	}
+
+}
---
+++ Practice2Search.java
@@ -0,0 +1,6 @@
+public interface Practice2Search {
+
+	public String searchName();
+	public int search(int [] arr, int target);
+}
+
